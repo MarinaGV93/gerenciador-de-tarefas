2fc26ef458e2f5583be8f04fee8c1ff90430d246
MarinaGV93 <marinasgv@hotmail.com>
2022-05-22 21:37:38 -0300

    Adiciona funcionalidade de criar tarefas

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,14 +1,32 @@
 # arquivo principal, onde iremos escrever o código da nossa aplicação
 
 # Criando a aplicação
-from curses.ascii import TAB
+from enum import Enum
+from uuid import UUID, uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel, constr
 
 # Definir Tarefas
 TAREFAS = []
 
 app = FastAPI()
+
+
+class EstadosPossiveis(str, Enum):
+    finalizado = "finalizado"
+    nao_finalizado = "não finalizado"
+
+
+class TarefaEntrada(BaseModel):
+    titulo: constr(min_length=3, max_length=50)
+    descricao: constr(max_length=140)
+    estado: EstadosPossiveis = EstadosPossiveis.nao_finalizado
+
+
+class Tarefa(TarefaEntrada):
+    id: UUID
+
 
 # Uma função para listar as tarefas
 # Toda vez que chamar o verbo get em tarefas, irá executar esse codigo, por causa do @
@@ -16,3 +34,13 @@ app = FastAPI()
 def listar():
     # [] = uma lista de tarefas que esta vazia
     return TAREFAS
+
+
+@app.post(
+    "/tarefas", response_model=Tarefa, status_code=status.HTTP_201_CREATED
+)
+def criar(tarefa: TarefaEntrada):
+    nova_tarefa = tarefa.dict()
+    nova_tarefa.update({"id": uuid4()})
+    TAREFAS.append(nova_tarefa)
+    return nova_tarefa
--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -31,16 +31,17 @@ def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
     # isinstance = verifica se aquilo é uma lista. Pega a resposta.json e tranforma em formato lista
     assert isinstance(resposta.json(), list)
 
-    def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
-        # Adicionando uma tarefa na lista
-        TAREFAS.append(
-            {
-                "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                "titulo": "titulo 1",
-                "descricao": "descricao 1",
-                "estado": "finalizado",
-            }
-        )
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
+    # Adicionando uma tarefa na lista
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
 
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
@@ -51,3 +52,129 @@ def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
 
     # Colocar a aplicação no ar:
     # uvicorn --reload gerenciador_tarefas.gerenciador:app
+
+    # Implementação do sistema
+    # git push heroku main
+
+
+def test_recurso_tarefas_deve_aceitar_o_verbo_post():
+    cliente = TestClient(app)
+    resposta = cliente.post("/tarefas")
+    assert resposta.status_code != status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_titulo():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "titulo" in resposta.json().pop()
+    TAREFAS.clear()
+
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_descricao():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "descricao" in resposta.json().pop()
+    TAREFAS.clear()
+
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_um_estado():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert "estado" in resposta.json().pop()
+    TAREFAS.clear()
+
+
+def test_quando_uma_tarefa_e_submetida_deve_possuir_um_titulo():
+    cliente = TestClient(app)
+    resposta = cliente.post("/tarefas", json={})
+    assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_titulo_da_tarefa_deve_conter_entre_3_e_50_caracteres():
+    cliente = TestClient(app)
+    resposta = cliente.post("/tarefas", json={"titulo": 2 * "*"})
+    assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    resposta = cliente.post("/tarefas", json={"titulo": 51 * "*"})
+    assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_quando_uma_tarefa_e_submetida_deve_possuir_uma_descricao():
+    cliente = TestClient(app)
+    resposta = cliente.post("/tarefas", json={"titulo": "titulo"})
+    assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_descricao_da_tarefa_pode_conter_no_maximo_140_caracteres():
+    cliente = TestClient(app)
+    resposta = cliente.post(
+        "/tarefas", json={"titulo": "titulo", "descricao": "*" * 141}
+    )
+    assert resposta.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_quando_criar_uma_tarefa_a_mesma_deve_ser_retornada():
+    cliente = TestClient(app)
+    tarefa_esperada = {"titulo": "titulo", "descricao": "descricao"}
+    resposta = cliente.post("/tarefas", json=tarefa_esperada)
+    tarefa_criada = resposta.json()
+    assert tarefa_criada["titulo"] == tarefa_esperada["titulo"]
+    assert tarefa_criada["descricao"] == tarefa_esperada["descricao"]
+    TAREFAS.clear()
+
+
+def test_quando_criar_uma_tarefa_seu_id_deve_ser_unico():
+    cliente = TestClient(app)
+    tarefa1 = {"titulo": "titulo1", "descricao": "descricao1"}
+    tarefa2 = {"titulo": "titulo2", "descricao": "descricao1"}
+    resposta1 = cliente.post("/tarefas", json=tarefa1)
+    resposta2 = cliente.post("/tarefas", json=tarefa2)
+    assert resposta1.json()["id"] != resposta2.json()["id"]
+    TAREFAS.clear()
+
+
+def test_quando_criar_uma_tarefa_seu_estado_padrao_e_nao_finalizado():
+    cliente = TestClient(app)
+    tarefa = {"titulo": "titulo", "descricao": "descricao"}
+    resposta = cliente.post("/tarefas", json=tarefa)
+    assert resposta.json()["estado"] == "não finalizado"
+    TAREFAS.clear()
+
+
+def test_quando_criar_uma_tarefa_codigo_de_status_retornado_deve_ser_201():
+    cliente = TestClient(app)
+    tarefa = {"titulo": "titulo", "descricao": "descricao"}
+    resposta = cliente.post("/tarefas", json=tarefa)
+    assert resposta.status_code == status.HTTP_201_CREATED
+    TAREFAS.clear()
+
+
+def test_quando_criar_uma_tarefa_esta_deve_ser_persistida():
+    cliente = TestClient(app)
+    tarefa = {"titulo": "titulo", "descricao": "descricao"}
+    cliente.post("/tarefas", json=tarefa)
+    assert len(TAREFAS) == 1
+    TAREFAS.clear()
